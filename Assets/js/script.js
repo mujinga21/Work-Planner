d7177507595e97eb59b355e06dbeeb90aac8751d
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-03-27 15:59:04 -0400

    my first draft

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+//   /var today = moment();
+var today = moment();
+$("#1a").text(today.format("MMM Do, YYYY"));
+
+//   var daily = localStorage.getItem(8);
+//   $("#time-block >input").val(daily);
+// });
+
+// $(".saveBtn").on("click", function () {
+//   console.log($(this));
+//   // console.log($("#time-block>input").val(), $("#hour").text());
+//   localStorage.setItem($("#hour").text(), $("#time-block>input").val());
