32ffa757031606b41f788526eaa9f2a4c3671e89
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-03-28 00:05:34 -0400

    last upload

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,43 +2,19 @@
 var today = moment();
 $("#1a").text(today.format("MMM Do, YYYY"));
 
-var schedule = document.getElementById("time-block");
-var daily = document.getElementById("hour");
-var saveButton = document.getElementById("save");
+// var  activity = localStorage.getItem("hour");
 
-saveButton.addEventListener("click", function (event) {
-  event.preventDefault();
+var daily = localStorage.getItem("hour");
+var schedule = localStorage.getItem("comment");
 
-  var busy = {
-    hour: hour.value,
-    comment: comment.value.trim(),
-  };
+$("#comment >input").val(schedule);
 
-  localStorage.setItem("busy", JSON.stringify(studentGrade));
-  renderMessage();
-});
+$(".saveBtn").on("click"),
+  // , function (event)
+  // {
+  //   event.preventDefault();
+  //   {
+  console.log($("#time-block").val(), $("#hour").text);
 
-function renderMessage() {
-  var nextActivity = JSON.parse(localStorage.getItem("busy"));
-  if (nextActivity !== null) {
-    document.querySelector(".message").textContent = getReady;
-  }
-}
-
-// // // var  activity = localStorage.getItem("hour");
-
-// // var daily = localStorage.getItem(8);
-// // // $("#comment >input").val(daily);
-// // $(".saveBtn").on("click", function () {
-// //   console.log($(this));
-// //   localStorage.setItem($("#hour").text(), $("#comment>").value());
-// // });
-// var daily = localStorage.getItem(hour);
-// $("#comment >input").val(daily);
-// $console.log($("#time-block").text)
-// // $(".saveBtn").on("click", function () {
-
-//   // console.log($(this));
-
-//   // localStorage.setItem($("#hour").text(), $("#comment>").value());
-// });
+localStorage.setItem("comment", schedule);
+localStorage.setItem("hour", daily);
